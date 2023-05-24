0b439e4455d7f6feea3e16fa1212af6a96c0a1c7
ReemAlmamdouh <reem.almamdouh@gmail.com>
2023-05-24 13:06:20 -0700

    update

---
+++ r_stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * r_stack - prints top
+ * @head: stack head
+ * @line_counter: line_number
+ * Return: no return
+*/
+void r_stack(stack_t **head, unsigned int line_counter)
+{
+	(void)head;
+	(void)line_counter;
+	sub.lifi = 0;
+}
