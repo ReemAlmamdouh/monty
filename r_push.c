de28485bbaed2c238e5557b747bd0e68c91403c7
ReemAlmamdouh <reem.almamdouh@gmail.com>
2023-05-25 05:31:50 -0700

    update

--- r_push.c
+++ r_push.c
@@ -12,7 +12,7 @@ int n, f = 0, glab = 0;
 if (sub.arg)
 {
 if (sub.arg[0] == '-')
-                        f++;
+f++;
 for (; sub.arg[f] != '\0'; f++)
 {
 if (sub.arg[f] > 57 || sub.arg[f] < 48)
@@ -24,11 +24,12 @@ free(sub.lncont);
 free_stack(*head);
 exit(EXIT_FAILURE); }}
 else
-{ fprintf(stderr, "L%d: usage: push integer\n", line_number);
+{fprintf(stderr, "L%d: usage: push integer\n", line_number);
 fclose(sub.file);
 free(sub.lncont);
 free_stack(*head);
-exit(EXIT_FAILURE); }
+exit(EXIT_FAILURE);
+}
 n = atoi(sub.arg);
 if (sub.lifi == 0)
 addnod(head, n)
